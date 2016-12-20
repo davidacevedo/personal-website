5ea0dda7c1765aa45f816f1737d98b90ca4932af
David Acevedo <david.acevedo1@citrix.com>
2016-12-19 20:03:08 -0800

    Add webpack and a dist directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import styles from './styles.scss';
 console.log('compiled');
+console.log(styles);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
 
   output: {
-    path: `${__dirname}/../dist/public`,
+    path: `${__dirname}/../dist`,
     filename: 'bundle.js',
     publicPath: '/',
   },
